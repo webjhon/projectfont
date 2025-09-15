1b573e708d5fe7be83dfd9a7f65857f1e81d09b2
webjhon <colorado.santos@gmail.com>
2025-09-15 19:20:00 -0300

    Front End Versão 1.0
    Arquivos iniciais do desenvolvimento.

---
+++ script.js
@@ -0,0 +1,8 @@
+function salvarDados() {
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let telefone = document.getElementById("telefone").value;
+  let senha = document.getElementById("senha").value;
+  // Dados armazenados em variáveis locais
+  // (a lógica de integração será pela equipe  POO)
+}
